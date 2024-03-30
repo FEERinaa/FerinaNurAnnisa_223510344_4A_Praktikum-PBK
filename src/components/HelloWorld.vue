e5de63db5d85450bbfcaa91538115b10b0405e36
ferina <ferinanurannisa05@gmail.com>
2024-03-30 20:27:44 +0700

    tugas praktikum pbk

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h4>
+      Halo Guys!<br />
+      Saya adalah seorang wanita independen. Wanita independen yang lebih mengutamakan pekerjaan
+      dibandingkan kegiatan yang kurang bermanfaat.<br />
+      Uang memang bukan segalanya, tapi segalanya butuh uang. Oleh karena itu, jika ada suatu
+      kegiatan yang menghasilkan uang yang halal, Jangan lupa untuk mengajak saya.<br />
+      Karena saya adalah orang yang bertanggungjawab atas tugas yang berikan.Selain itu, saya
+      berkepribadian yang jujur, ramah, dapat dipercaya, dan dapat memimpin ataupun dipimpin.
+      <!-- <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> + -->
+      <!-- <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. -->
+    </h4>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,82 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Tujuan</template>
+    Portofolio ini bertujuan untuk memenuhi tugas dari Pemrograman Berbasis Komponen atau dikenal
+    dengan PBK. Tugas PBK ini dilakukan dengan mengubah vue yang ada menjadi Portofolio atau CV. Di
+    sini kami dibebaskan untuk berkreasi mengenai isinya.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Profil</template>
+    <table id="data">
+      <tr>
+        <th>Nama Lengkap</th>
+        <th>Nama Panggilan</th>
+        <th>TTL</th>
+        <th>Golongan Darah</th>
+      </tr>
+
+      <tr>
+        <td>Ferina Nur Annisa</td>
+        <td>Ferina</td>
+        <td>Padang, 05 Januari 2004</td>
+        <td>B-</td>
+      </tr>
+    </table>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Deskripsi Karakteristik</template>
+    Sebagai wanita independen, saya tak terlepas dari karakteristik yang melekat di diri, sebagai
+    berikut :<br />
+    <ul>
+      <li>bertanggungjawab</li>
+      <li>Jujur</li>
+      <li>Amanah</li>
+      <li>Ramah</li>
+    </ul>
+    dan sebagainya yang menunjang sebuah pekerjaan terlaksana.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Pengalaman</template>
+    <ol>
+      <li>Volunteer dalam kegiatan IT FUNDAY 2023 sebagai Pamong</li>
+      <li>Volunteer dalam kegiatan FUTSAL CUP 4 2023 di devisi Acara</li>
+      <li>Volunteer dalam kegiatan Seminar BITS LAB 2023 di devisi Acara</li>
+      <li>Volunteer dalam kegiatan MILAD TI UIR KE-16 2023 sebagai CO Acara</li>
+      <li>Sedang Menjabat di Organisasi HIMATIF sebagai Bendahara Umum</li>
+    </ol>
+    dan organisasi lainnya.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Hubungi</template>
+    WA : 08-58-....<br />
+    IG :
+  </WelcomeItem>
+</template>
